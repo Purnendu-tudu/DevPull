b79ba4c37fdddb00abc58e051fe2f890c21fc693
purnendu-tudu <purnendu2udu@gmail.com>
2024-08-12 18:57:53 +0530

    fix som issues

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,10 @@
 import { createRouteHandler } from "uploadthing/server";
 import { fileRouter } from "./core";
+import { NextRequest } from "next/server";
 
-export const { GET, POST } = createRouteHandler({
+const { GET: rawGET, POST: rawPOST } = createRouteHandler({
   router: fileRouter,
 });
+
+export const GET = (request: NextRequest) => rawGET(request as any);
+export const POST = (request: NextRequest) => rawPOST(request as any);
